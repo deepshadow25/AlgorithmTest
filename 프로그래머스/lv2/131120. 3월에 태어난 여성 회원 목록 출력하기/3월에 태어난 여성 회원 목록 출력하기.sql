acf8203e8f27be91a2d6ab0e01ff7094fe841a37
Seongyeon <115054681+deepshadow25@users.noreply.github.com>
2023-04-26 13:18:11 +0900

    Update 3월에 태어난 여성 회원 목록 출력하기.sql

--- 프로그래머스/lv2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/lv2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -3,3 +3,4 @@ SELECT member_id, member_name, gender, date_format(date_of_birth, '%Y-%m-%d') as
 from member_profile
 where month(date_of_birth) = 3 and TLNO is not null and gender = 'W'
 order by member_id;
+-- date_of_birth가 date type, format을 지정해 줘야 함. 그렇지 않으면 다르게 출력되는듯?
