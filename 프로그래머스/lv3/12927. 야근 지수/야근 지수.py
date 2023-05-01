c517516a4c83f52256fba78207a1a72f7b8eda5b
Seongyeon <115054681+deepshadow25@users.noreply.github.com>
2023-05-02 01:47:15 +0900

    [level 3] Title: 야근 지수, Time: 0.72 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.py
@@ -1,11 +1,31 @@
-import heapq
+from collections import deque
 
 def solution(n, works):
-    if n > sum(works):
+    work_hour, works = n, deque(sorted(works))
+
+    def overtime_required():
+        return True if sum([hour for hour in works]) > n else False
+    
+    if not overtime_required():
         return 0
-    heap = []
-    for i in works:
-        heapq.heappush(heap,-i)
-    for i in range(n):
-        heapq.heappush(heap,heapq.heappop(heap)+1)
-    return sum([i**2 for i in heap])
+
+    m_val, m_cnt = works.pop(), 1
+    while works and work_hour > 0:
+        new_value = works[-1]
+        v_diff = m_val - new_value
+        wh_need = v_diff * m_cnt
+        if wh_need <= work_hour:
+            work_hour -= wh_need
+            m_val = works.pop()
+            m_cnt += 1
+        else:
+            break
+
+    m_val -= work_hour // m_cnt
+    r_work_hour = work_hour % m_cnt
+    answer = (m_val ** 2) * (m_cnt - r_work_hour) + ((m_val - 1) ** 2) * r_work_hour
+
+    for w in works:
+        answer += w ** 2
+
+    return answer
